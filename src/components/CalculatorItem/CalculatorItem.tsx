2b0193441fae95c60764d8e03c38da0e69348360
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-07-13 18:09:56 +0600

    replaced the "<" sign with an icon in the CalculatorItem.tsx file

--- src/components/CalculatorItem/CalculatorItem.tsx
+++ src/components/CalculatorItem/CalculatorItem.tsx
@@ -9,10 +9,20 @@ interface Props {
 const CalculatorItem: React.FC<Props> = ({ title, width, onClick }) => (
   <button
     style={{ width, height: 60, letterSpacing: title === '-' ? -2.5 : 0 }}
-    className="d-flex justify-content-center align-items-center fs-1 border border-2 border-info rounded-3 ms-2 mb-2"
+    className="
+    d-flex justify-content-center align-items-center
+    fs-1
+    border border-2 border-info rounded-3
+    ms-2 mb-2"
     onClick={onClick}
   >
-    {title === '-' ? '--' : title}
+    {
+      title === '<' ?
+          <img className="w-75" src="https://cdn-icons-png.flaticon.com/512/61/61167.png" alt=""/> :
+          title === '-' ?
+              '--' :
+              title
+    }
   </button>
 );
 
